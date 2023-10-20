b3a0841cc2e951d45b996149bd29056472a99bbc
MariiaSam <kontragent@ukr.net>
2023-10-21 01:23:43 +0300

    create ex-modal

---
+++ src/js/exercuse-modal-create.js
@@ -0,0 +1,97 @@
+//  {
+
+//     "_id": "64f389465ae26083f39b17a4",
+//     "bodyPart": "waist",
+//     "equipment": "body weight",
+//     "gifUrl": "https://ftp.goit.study/img/power-pulse/gifs/0003.gif",
+//     "name": "air bike",
+//     "target": "abs",
+//     "description": "This refers to your core muscles, which include the rectus abdominis, obliques, and transverse abdominis. They're essential for maintaining posture, stability, and generating force in many movements. Exercises that target the abs include crunches, leg raises, and planks.",
+//     "rating": 4.33,
+//     "burnedCalories": 312,
+//     "time": 3,
+//     "popularity": 4
+//   }
+
+function renderExerciseModal({
+  bodyPart,
+  equipment,
+  gifUrl,
+  name,
+  target,
+  description,
+  rating,
+  burnedCalories,
+  time,
+  popularity,
+}) {
+  return ` 
+    <div class="exercise-modal-tumb">
+            <img
+              class="exercise-modal__img"
+              src="${gifUrl}" 
+              alt="exercise"
+              width="295"
+              height="259"
+            />
+            <div class="exercise-modal-tumb_card">
+              <h2 class="exercise-modal__title">${makeUpperCaseFirst(name)}</h2>
+              <p class="exercise-modal-rating__number">${rating}</p>
+              <ul class="exercise-modal-list">
+                <li class="exercise-modal-list-item">
+                  <h3 class="exercise-modal-list__title">Target</h3>
+                  <p class="exercise-modal-list__text">${makeUpperCaseFirst(
+                    target
+                  )}</p>
+                </li>
+                <li class="exercise-modal-list-item">
+                  <h3 class="exercise-modal-list__title">Body Part</h3>
+                  <p class="exercise-modal-list__text">${makeUpperCaseFirst(
+                    bodyPart
+                  )}</p>
+                </li>
+                <li class="exercise-modal-list-item">
+                  <h3 class="exercise-modal-list__title">Equipment</h3>
+                  <p class="exercise-modal-list__text">${makeUpperCaseFirst(
+                    equipment
+                  )}</p>
+                </li>
+                <li class="exercise-modal-list-item">
+                  <h3 class="exercise-modal-list__title">Popular</h3>
+                  <p class="exercise-modal-list__text">${popularity}</p>
+                </li>
+                <li class="exercise-modal-list-item">
+                  <h3 class="exercise-modal-list__title">Burned calories</h3>
+                  <p class="exercise-modal-list__text">${burnedCalories}/${time}min</p>
+                </li>
+              </ul>
+              <p class="exercise-modal__description">
+               ${makeUpperCaseFirst(description)}
+              </p>
+            </div>
+          </div>
+          <ul class="exercise-modal-button">
+            <li class="exercise-modal_item">
+              <button
+                type="button"
+                class="exercise-modal-favorite__button">
+                Add to favorites
+                <span>
+                  <svg class="modal-exercise-heart-icon">
+                      <use href="../img/icons.svg#icon-heart" width="18" height="18"></use>
+                  </svg>
+                </span>
+              </button>
+            </li>
+
+            <li class="exercise-modal_item">
+<button type="button" class="exercise-modal-button__rating">
+              Give a rating
+            </button>
+            </li>
+          </ul>`;
+}
+
+function makeUpperCaseFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1); //Метод charAt() возвращает указанный символ из строки.
+}
