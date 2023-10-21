f3bd2ba56cc8bf50d3b8deaaa2119c43bc6d45aa
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-10-21 14:24:17 +0300

    add

--- src/js/filter-cards.js
+++ src/js/filter-cards.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { fetchFilter } from './api';
 import { createFilterString } from './components/filter-card-template';
-import { refs } from './refs';
+import { listRef } from './refs';
 
 getFilters();
 
@@ -10,8 +10,5 @@ async function getFilters() {
   if (screen.width > 767) {
     data = await fetchFilter(1, 12, 'Body parts');
   }
-  refs.listRef.insertAdjacentHTML(
-    'beforeend',
-    createFilterString(data.results)
-  );
+  listRef.insertAdjacentHTML('beforeend', createFilterString(data.results));
 }
