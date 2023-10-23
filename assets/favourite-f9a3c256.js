38d962600b000a4cf2b5d30e31e662d7bec18ea5
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-10-23 09:42:51 +0000

    Deploying to gh-pages from @ lizaklimova/your_energy_app@02f8eb58153b191fbea230a77d771e08ad4206ed 🚀

--- assets/favourite-f9a3c256.js
+++
@@ -1,1 +0,0 @@
-import{a as r,f as o}from"./classFunctions-3f35fbfe.js";let l={};const c=()=>{const t=Object.keys(localStorage);if(t.length>=1&&localStorage.getItem("quote")){const a=t.filter(e=>e!=="quote");for(let e of a){try{const s=localStorage.getItem(e);l=JSON.parse(s)}catch{error}r(o,"is-hidden")}}else{r(o,"favourite__text");return}};window.addEventListener("load",c);
