2671ba67dc7ce63a50c184ba78f329e546daa3c7
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-10-24 10:18:07 +0000

    Deploying to gh-pages from @ lizaklimova/your_energy_app@a8408cec88d7f79f28be6abe07a671f17d59bc25 🚀

--- assets/index-575d87c0.js
+++ assets/index-575d87c0.js
@@ -16,7 +16,7 @@ background-repeat: no-repeat;
  <p class="exercises__filter-category">${q(r)}</p>
     <p class="exercises__filter-name">${o}</p>
 </div>
-  </li>`).join("")}function ft(s){let n="";for(let i=0;i<s;i++)n+='<li class="exercises__skeleton-loader"><span class="skeleton-loader"><symbol id="icon-image" viewBox="0 0 32 32"><path d="M29.996 4c0.001 0.001 0.003 0.002 0.004 0.004v23.993c-0.001 0.001-0.002 0.003-0.004 0.004h-27.993c-0.001-0.001-0.003-0.002-0.004-0.004v-23.993c0.001-0.001 0.002-0.003 0.004-0.004h27.993zM30 2h-28c-1.1 0-2 0.9-2 2v24c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-24c0-1.1-0.9-2-2-2v0z"></path><path d="M26 9c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3z"></path><path d="M28 26h-24v-4l7-12 8 10h2l7-6z"></path></symbol><span></span></span></li>';return n}function we(s){const n=s.map(({bodyPart:i,name:o,target:r,rating:c,burnedCalories:f,time:G})=>`<li class="exercises__item-card">
+  </li>`).join("")}function ft(s){let n="";for(let i=0;i<s;i++)n+='<li class="exercises__skeleton-loader"><span class="skeleton-loader"><symbol class="exercises__image-skeleton" id="icon-image" viewBox="0 0 32 32"><path d="M29.996 4c0.001 0.001 0.003 0.002 0.004 0.004v23.993c-0.001 0.001-0.002 0.003-0.004 0.004h-27.993c-0.001-0.001-0.003-0.002-0.004-0.004v-23.993c0.001-0.001 0.002-0.003 0.004-0.004h27.993zM30 2h-28c-1.1 0-2 0.9-2 2v24c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-24c0-1.1-0.9-2-2-2v0z"></path><path d="M26 9c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3z"></path><path d="M28 26h-24v-4l7-12 8 10h2l7-6z"></path></symbol><span></span></span></li>';return n}function we(s){const n=s.map(({bodyPart:i,name:o,target:r,rating:c,burnedCalories:f,time:G})=>`<li class="exercises__item-card">
         <div class="card">
           <div class="card__top">
             <span class="card_tag">Workout</span>
@@ -95,8 +95,8 @@ background-repeat: no-repeat;
                 class="exercise-modal-button__favorite">
                 Add to favorites
                 <span>
-                 Copy
-<svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" viewBox="0 0 20 18" fill="none">
+              
+<svg class="modal-exercise-heart-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="18" viewBox="0 0 20 18" fill="none">
 <path d="M17.3666 2.84172C16.941 2.41589 16.4356 2.0781 15.8794 1.84763C15.3232 1.61716 14.727 1.49854 14.1249 1.49854C13.5229 1.49854 12.9267 1.61716 12.3705 1.84763C11.8143 2.0781 11.3089 2.41589 10.8833 2.84172L9.99994 3.72506L9.1166 2.84172C8.25686 1.98198 7.0908 1.49898 5.87494 1.49898C4.65907 1.49898 3.49301 1.98198 2.63327 2.84172C1.77353 3.70147 1.29053 4.86753 1.29053 6.08339C1.29053 7.29925 1.77353 8.46531 2.63327 9.32506L3.5166 10.2084L9.99994 16.6917L16.4833 10.2084L17.3666 9.32506C17.7924 8.89943 18.1302 8.39407 18.3607 7.83785C18.5912 7.28164 18.7098 6.68546 18.7098 6.08339C18.7098 5.48132 18.5912 4.88514 18.3607 4.32893C18.1302 3.77271 17.7924 3.26735 17.3666 2.84172Z" stroke="#242424" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
                 </span>
