6b968c8b2b8ca425bd2bc228d73ac17fd378e2ae
AnastasiaGOIT <hudzevych.music@gmail.com>
2023-10-26 17:30:29 +0300

    icon fix

--- src/js/components/mode.js
+++ src/js/components/mode.js
@@ -1,3 +1,16 @@
+import iconSun from '../../img/icons.svg';
+const iconDiv = document.querySelector('.btn__icon-container');
+
+const iconMarkup = `  <svg class="btn__icon">
+              <use
+                class="icon"
+                href="${iconSun}#icon-brightness-contrast"
+                width="14"
+                height="14"
+              ></use>
+            </svg>`;
+iconDiv.insertAdjacentHTML('beforeend', iconMarkup);
+
 const body = document.querySelector('body');
 const btn = document.querySelector('.btn');
 const icon = document.querySelector('.btn__icon');
