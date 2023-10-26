9bd411a2d20e993fce25de9e7b3537fc83c89a5c
AnastasiaGOIT <hudzevych.music@gmail.com>
2023-10-26 21:06:12 +0300

    icon

--- src/js/components/mode.js
+++ src/js/components/mode.js
@@ -3,7 +3,7 @@ const iconDiv = document.querySelector('.btn__icon-container');
 
 const iconMarkup = `  <svg class="btn__icon">
               <use
-                class="icon"
+                class="btn__icon"
                 href="${iconSun}#icon-brightness-contrast"
                 width="14"
                 height="14"
